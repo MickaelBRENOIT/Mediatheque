43500d63d0205068eaee598c0ab96fe60969c144
Mickael <brenoit.mickael@gmail.com>
2017-12-29 19:45:57 +0100

    Edit employee is working now

--- mediatheque-group3/src/main/java/com/mickaelbrenoit/web/controller/AdminController.java
+++ mediatheque-group3/src/main/java/com/mickaelbrenoit/web/controller/AdminController.java
@@ -2,11 +2,15 @@ package com.mickaelbrenoit.web.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.mickaelbrenoit.business.model.User;
 import com.mickaelbrenoit.business.service.RoleService;
 import com.mickaelbrenoit.business.service.UserService;
+import com.mickaelbrenoit.utils.PasswordUtils;
 
 @Controller
 @RequestMapping("/admin")
@@ -35,4 +40,30 @@ public class AdminController {
 		return "admin/listofemployees";
 	}
 	
+	@RequestMapping(value="/editemployee", method = RequestMethod.GET)
+	public String editEmployee(@RequestParam("id") Long id, Model model) {
+		model.addAttribute("user", userService.findById(id));
+		model.addAttribute("role", roleService.findByName("EMP"));
+		return "admin/editemployee";
+	}
+	
+	@RequestMapping(value="/editemployee", method = RequestMethod.POST)
+	public String formEditEmployee(@Valid @ModelAttribute User user, BindingResult bindingResult, Model model) {
+		
+		User alreadyExists = userService.findByLogin(user.getLogin());
+		
+		if (bindingResult.hasErrors() || alreadyExists != null) {
+			model.addAttribute("role", roleService.findByName("EMP"));
+			if(alreadyExists != null) {
+				model.addAttribute("loginalreadyexists", user.getLogin());
+			}
+            return "admin/editemployee";
+        }
+
+		user.setPassword(PasswordUtils.hashPassword(user.getPassword()));
+		
+		userService.save(user);
+		
+		return "redirect:/admin/listemployees";
+	}
 }
